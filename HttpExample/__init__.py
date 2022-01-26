6fbad67c4cf282a4262c1120cb769e01b312a91b
Ahmed Bilal <ahdbilal@gmail.com>
2022-01-26 13:27:52 -0800

    added gha specific code

--- HttpExample/__init__.py
+++ HttpExample/__init__.py
@@ -5,26 +5,32 @@ import azure.functions as func
 import json
 import requests
 
+REPO_URL = ""   # eg. org/repo
+GHTOKEN = ""    #github token
+EVENT_NAME = "" #mandatory name for the event. can be dynamically provided  
+
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
     #HTTP url
-    url = ""
+    url = "https://api.github.com/repos/" + REPO_URL + "dispatches"
 
     #HTTP header
     headers = {
-        'Authorization': '',
+        'Authorization': 'Bearer',
         'Content-Type': 'text/plain'
         }
 
     #HTTP body
     body = dict()
+    body['event_type'] = EVENT_NAME
     body['client_payload'] = req.get_json()
     json_body = (json.dumps(body)).encode('utf-8')
-    
+
     requests.request("POST", url, headers=headers, data=json_body)
 
     return func.HttpResponse(
             status_code=200
         )
+
